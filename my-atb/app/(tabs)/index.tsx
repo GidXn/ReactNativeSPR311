7355e304cf1b805dacf31d45327e02f26310cbe8
MichaelGydin <michael.gydin@gmail.com>
2025-10-08 15:10:25 +0300

    Зроблено сторінку для входу

--- my-atb/app/(tabs)/index.tsx
+++ my-atb/app/(tabs)/index.tsx
@@ -20,6 +20,10 @@ export default function HomeScreen() {
         // console.log("Press button not found");
     }
 
+    const onPressLogin = () => {
+        router.push("/login");
+    }
+
     return (
         <>
             <SafeAreaProvider>
@@ -48,6 +52,13 @@ export default function HomeScreen() {
                                     onPress={onPressNotFound}
                                 >
                                     <Text className="text-white font-bold text-lg">{"Перехід на not found"}</Text>
+                                </TouchableOpacity>
+
+                                <TouchableOpacity
+                                    className={`bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 `}
+                                    onPress={onPressLogin}
+                                >
+                                    <Text className="text-white font-bold text-lg">{"Перехід на вхід"}</Text>
                                 </TouchableOpacity>
 
                             </View>
