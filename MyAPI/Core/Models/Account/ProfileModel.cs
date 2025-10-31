00acf01832830be5fbf1e7f2b0561c242dd58302
MichaelGydin <michael.gydin@gmail.com>
2025-10-31 14:18:31 +0200

    На сервері зроблено метод контролера, який видає інформацію про авторизованого користувача

---
+++ MyAPI/Core/Models/Account/ProfileModel.cs
@@ -0,0 +1,13 @@
+namespace Core.Models.Account;
+
+public class ProfileModel
+{
+    public long Id { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Image { get; set; }
+}
+
+
